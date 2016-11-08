d97584621ddcde84f6cd168976ef87f10ae88d18
zcmack <christophermack@gmail.com>
2016-11-07 20:31:27 -0600

    added additional route to search setlists

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 import requests, json
 
 app = Flask(__name__)
@@ -8,14 +8,23 @@ app = Flask(__name__)
 def index():
     return "setlist.fm proxy api"
 
-@app.route('/api/search/<string:artist>')
-def search(artist):
+@app.route('/api/search/artist/<string:artist>')
+def artistSearch(artist):
     #artist = 'M83'
     url = 'http://api.setlist.fm/rest/0.1/search/artists.json?artistName='+artist
     response = requests.get(url)
     if(response.ok):
         parsedResponse = json.loads(response.content)
-        return(parsedResponse["artists"]["artist"][0]["@name"])
+        return(jsonify(parsedResponse["artists"]["artist"]))
+
+@app.route('/api/search/setlists/<string:mbid>')
+def setlistSearch(mbid):
+    url = 'http://api.setlist.fm/rest/0.1/search/setlists.json?artistMbid='+mbid
+    response = requests.get(url)
+    if(response.ok):
+        parsedResponse = json.loads(response.content)
+        return(jsonify(parsedResponse))
+
 
 
 if __name__ == '__main__':
